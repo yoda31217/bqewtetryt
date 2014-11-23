f7f058a81cb8ef8aa007b528d72426295dc5ef79
yoda31217 <jbourne.nik@gmail.com>
2014-11-23 23:49:42 +0200

    #46 extremum calculations added

--- lucky/bmbets/bmbets_normalize_odds.js
+++ lucky/bmbets/bmbets_normalize_odds.js
@@ -1,55 +1,79 @@
-var normalizeOddRow = function (row, rightIndex) {
+function parsePercent(cell) {
+  var percent = cell.innerText;
+  return Number(percent.substring(0, percent.length - 1)) / 100.0;
+}
+
+function calculateFactor(cell) {
+  var percent = parsePercent(cell);
+  return 1.0 / percent;
+}
+
+function calculateNormalOdd(cell, factor) {
+  var odd = Number(cell.innerText);
+  return odd * factor;
+}
+
+function processOddRow(row, minOdds, maxOdds, preMaxOdds) {
   var cells = row.children;
 
-  var percentCell = cells[cells.length - rightIndex];
-  var percent = percentCell.innerText;
-  percent = Number(percent.substring(0, percent.length - 1)) / 100.0;
+  var factor = calculateFactor(cells[cells.length - 2]);
 
-  var factor = 1.0 / percent;
+  for (var i = 1; i < cells.length - 2; i++) {
 
-  for (var i = 1; i < cells.length - rightIndex; i++) {
-    var oddCell = cells[i];
-    var odd = Number(oddCell.innerText);
-    oddCell.innerHTML += '<div style="color: green">' + (odd * factor).toFixed(3) + '</div>'
+    var normalOdd = calculateNormalOdd(cells[i], factor);
+
+    if (minOdds[i] > normalOdd) {
+      minOdds[i] = normalOdd;
+    }
+    else if (maxOdds[i] < normalOdd) {
+      preMaxOdds[i] = maxOdds[i];
+      maxOdds[i] = normalOdd;
+    }
+    else if (preMaxOdds[i] < normalOdd) {
+      preMaxOdds[i] = normalOdd;
+    }
+
+    cells[i].innerHTML += '<div style="color: green">' + normalOdd.toFixed(3) + '</div>';
   }
+}
 
-  //console.log(row);
-};
-
-var accumulateExtremumOdds = function (row, rightIndex, maxs, mins) {
+function processAverageOddRow(row, minOdds, maxOdds, preMaxOdds) {
   var cells = row.children;
 
-  var percentCell = cells[cells.length - rightIndex];
-  var percent = percentCell.innerText;
-  percent = Number(percent.substring(0, percent.length - 1)) / 100.0;
+  var factor = calculateFactor(cells[cells.length - 1]);
 
-  var factor = 1.0 / percent;
+  for (var i = 1; i < cells.length - 1; i++) {
 
-  for (var i = 1; i < cells.length - rightIndex; i++) {
-    var oddCell = cells[i];
-    var odd = Number(oddCell.innerText);
-    oddCell.innerHTML += '<div style="color: green">' + (odd * factor).toFixed(3) + '</div>'
+    var normalOdd = calculateNormalOdd(cells[i], factor);
+
+    cells[i].innerHTML += '<div style="color: green">' + minOdds[i].toFixed(3) + '</div><div style="color: green">' + normalOdd.toFixed(3) +
+    '</div><div style="color: green">' + preMaxOdds[i].toFixed(3) + '</div><div style="color: green">' + maxOdds[i].toFixed(3) + '</div>';
+  }
+}
+
+function normalizeOdds() {
+  var oddsPanelEls = $('#odds-panel');
+  if (0 === oddsPanelEls.length || oddsPanelEls[0].classList.contains('lucky-odds-normalized')) return;
+
+  var minOdds = [];
+  var maxOdds = [];
+  var preMaxOdds = [];
+  for (var i = 0; i < 5; i++) {
+    minOdds[i] = 1000.0;
+    preMaxOdds[i] = 0.0;
+    maxOdds[i] = 0.1;
   }
 
-  //console.log(row);
-};
-
-var normalizeOdds = function () {
-  //console.log('Norm');
-
-  var oddsPanelEl = document.getElementById('odds-panel');
-  if (!oddsPanelEl || oddsPanelEl.classList.contains('lucky-odds-normalized')) return;
-
-  var oddRows = $('#odds-panel > div > table.odds-table > tbody > tr');
-  for (var i = 0; i < oddRows.length; i++) {
-    normalizeOddRow(oddRows[i], 2);
+  var oddRows = oddsPanelEls.find('> div > table.odds-table > tbody > tr');
+  for (var j = 0; j < oddRows.length; j++) {
+    processOddRow(oddRows[j], minOdds, maxOdds, preMaxOdds);
   }
 
-  var averageOddRows = $('#odds-panel > div > table.odds-table > tfoot > tr.average');
-  normalizeOddRow(averageOddRows[0], 1);
+  var averageOddRows = oddsPanelEls.find('> div > table.odds-table > tfoot > tr.average');
+  processAverageOddRow(averageOddRows[0], minOdds, maxOdds, preMaxOdds);
 
-  oddsPanelEl.classList.add('lucky-odds-normalized');
-  //console.log('Norm done');
-};
+  oddsPanelEls[0].classList.add('lucky-odds-normalized');
+}
 
+//normalizeOdds();
 window.setInterval(normalizeOdds, 2000);
